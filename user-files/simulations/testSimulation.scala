8d3aed750d323190a1b55e4db0bde7ddcbce831c
wkd3475 <wkd3475@gmail.com>
2021-07-29 19:29:19 +0900

    -

--- user-files/simulations/testSimulation.scala
+++ user-files/simulations/testSimulation.scala
@@ -21,7 +21,7 @@ class BasicSimulation extends Simulation {
 
   setUp(
     scn.inject(constantUsersPerSec(1000).during(1000.seconds)).throttle(
-      reachRps(600).in(300.seconds),
+      reachRps(1000).in(500.seconds),
       holdFor(10.seconds)
     ).protocols(httpProtocol)
   )
